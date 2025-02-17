c1e223a4b4124ab0d0c71945e691dfb5a1d1b12a
gbeard27 <grace@gobeard.com>
2025-02-16 20:34:27 -0500

    Initial Commit

---
+++ SummersWellnessFarm/SummersWellnessFarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummersWellnessFarmApp.swift
+//  SummersWellnessFarm
+//
+//  Created by Grace Beard on 2/16/25.
+//
+
+import SwiftUI
+
+@main
+struct SummersWellnessFarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SummersWellnessFarmTests/SummersWellnessFarmTests.swift
@@ -0,0 +1,17 @@
+//
+//  SummersWellnessFarmTests.swift
+//  SummersWellnessFarmTests
+//
+//  Created by Grace Beard on 2/16/25.
+//
+
+import Testing
+@testable import SummersWellnessFarm
+
+struct SummersWellnessFarmTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
